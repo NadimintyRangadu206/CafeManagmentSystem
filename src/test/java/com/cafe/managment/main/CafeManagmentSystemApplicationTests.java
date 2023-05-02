149f4f527ad51a7a18b614601f109906cbf256a9
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-05-02 17:36:32 +0530

    Uploading project

---
+++ src/test/java/com/cafe/managment/main/CafeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.managment.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
