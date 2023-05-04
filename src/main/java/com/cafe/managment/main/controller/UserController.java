66d2b3fc2dd129fa884908a06468161befb3b873
NadimintyRangadu206 <nadimintyrangadu206@gmail.com>
2023-05-04 10:33:21 +0530

    Implementing user saveApi

--- src/main/java/com/cafe/managment/main/controller/UserController.java
+++ src/main/java/com/cafe/managment/main/controller/UserController.java
@@ -1,12 +1,43 @@
 package com.cafe.managment.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cafe.managment.main.constatnts.ErrorMessages;
+import com.cafe.managment.main.constatnts.SuccessMessages;
+import com.cafe.managment.main.exception.CafeException;
+import com.cafe.managment.main.model.User;
+import com.cafe.managment.main.request.UserRequest;
+import com.cafe.managment.main.response.ResponseObject;
+import com.cafe.managment.main.service.UserService;
 
 @RestController
 @RequestMapping("api/v1/user/")
 public class UserController {
-	
-	
+
+	@Autowired
+	public UserService userService;
+
+	@PostMapping("save/users")
+	public ResponseObject saveUser(@RequestBody UserRequest userRequest) {
+
+		ResponseObject responseObject = null;
+		 
+		User user = userService.saveUser(userRequest);
+		try {
+			if (user != null) {
+				responseObject = new ResponseObject(SuccessMessages.USER_IS_ADDED_SUCCESSFULLY, HttpStatus.OK);
+			} else {
+				responseObject = new ResponseObject(null, ErrorMessages.SOMETTHING_WENT_WRONG, HttpStatus.BAD_REQUEST);
+			}
+		} catch (CafeException e) {
+			responseObject = new ResponseObject(null, e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return responseObject;
+	}
 
 }
--- src/main/java/com/cafe/managment/main/repository/UserRepository.java
+++ src/main/java/com/cafe/managment/main/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.cafe.managment.main.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cafe.managment.main.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+ @Query(value="select email_id,id,password,status,role,user_name from user u where u.email_id=?", nativeQuery=true)
+ Optional<User> findByEmailId(String emailId);
 
 }
